6355b2c6481cad4a6626c93e121648704279394a
MageshVachiravel <mageshvachiravel@gmail.com>
2024-01-28 16:13:20 +0530

    docs:created my first react app

---
+++ src/components/App.js
@@ -0,0 +1,21 @@
+import Navbar from "./Navbar"
+import Search from "./Search"
+import Products from "./Products"
+import About from "./About"
+import Footer from "./Footer"
+
+
+function App(){
+    return (
+        <div>
+            <Navbar></Navbar>
+            <Search></Search>
+            <Products></Products>
+            <About></About>
+            <Footer></Footer>
+        </div>
+    )
+}
+
+export default App
+
---
+++ src/index.js
@@ -0,0 +1,7 @@
+import reactDOM from "react-dom/client"
+import "./style.css"
+import App from "./components/App"
+
+const root = reactDOM.createRoot(document.getElementById("root"))
+
+root.render(<App></App>)
